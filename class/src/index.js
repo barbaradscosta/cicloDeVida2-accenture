74bd647ea0750b76bd89d8182128a316d6c9d6ab
Barbara Costa <barbaradscosta14@gmail.com>
2021-07-13 19:45:40 -0300

    first commit

---
+++ class/src/index.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+class App extends React.Component{
+  state = {
+    clicou:"ainda não clicou",
+  }
+  componentDidMount(){
+    console.log('[App] - Apareceu na Tela')
+  }
+  handleClick(){
+    this.setState({
+      clicou: "clicou"
+    })
+  }
+  render(){
+    console.log("[App] - Renderizou")
+    return (
+      <div>
+        Olá, {this.props.name}, você {this.state.clicou}.
+        <button onClick={this.handleClick.bind(this)}>Clique aqui</button>
+      </div>
+    )
+  }
+}
+ReactDOM.render(
+  <React.StrictMode>
+    <App name={"Bárbara Costa"}/>
+  </React.StrictMode>,
+  document.getElementById('root')
+);
